c7ffc8391a0be5b7db4eee6dbb41b5aff2fed2c6
tremperb <55967770+tremperb@users.noreply.github.com>
2020-03-08 22:35:06 -0700

    fixed js and css

---
+++ public/clientjs.js
@@ -0,0 +1,16 @@
+function checkUserInput() {
+  var selectedInputs = document.getElementsByClassName('menu-select');
+  var check = true;
+  for(var i = 0; i < selectedInputs.length; i++) {
+    if(selectedInputs[i].value == "initial") {
+      check = false;
+    }
+
+  }
+  if(check == false) {
+    alert("You must fill out all options");
+  }
+  else {
+    //call function to add to database
+  }
+}
